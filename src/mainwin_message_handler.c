0d637ce2bb0d0a404669833063e3b092df5aa336
Marcel Gosmann <thafiredragonofdeath@gmail.com>
2024-07-05 21:37:52 +0200

    Added missing copyright and license statements

--- src/mainwin_message_handler.c
+++ src/mainwin_message_handler.c
@@ -1,3 +1,11 @@
+/*
+ * This file is part of µHashtools.
+ * µHashtools is a small graphical file hashing tool for Microsoft Windows.
+ * 
+ * SPDX-FileCopyrightText: 2024 Marcel Gosmann <thafiredragonofdeath@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #include "mainwin_message_handler.h"
 
 #include "errorutils.h"
--- src/mainwin_message_handler.h
+++ src/mainwin_message_handler.h
@@ -1,3 +1,11 @@
+/*
+ * This file is part of µHashtools.
+ * µHashtools is a small graphical file hashing tool for Microsoft Windows.
+ * 
+ * SPDX-FileCopyrightText: 2024 Marcel Gosmann <thafiredragonofdeath@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #pragma once
 
 #include <Windows.h>
