b6a75ff572dff786ac578c46b65362df7d2a8609
Marcel Gosmann <thafiredragonofdeath@gmail.com>
2025-04-12 17:06:39 +0200

    Added current year to copyright text in the executable resources.

--- src/product_common.h
+++ src/product_common.h
@@ -2,7 +2,7 @@
  * This file is part of µHashtools.
  * µHashtools is a small graphical file hashing tool for Microsoft Windows.
  * 
- * SPDX-FileCopyrightText: 2024 Marcel Gosmann <thafiredragonofdeath@gmail.com>
+ * SPDX-FileCopyrightText: 2024-2025 Marcel Gosmann <thafiredragonofdeath@gmail.com>
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
@@ -17,7 +17,7 @@
 #define UHASHTOOLS_RC_PRODUCTVERSION       0,3,0,0
 #define UHASHTOOLS_RC_PRODUCTVERSION_STR   L"0.3.0-SNAPSHOT\0"
 #define UHASHTOOLS_RC_COMPANY_STR          L"techdragonblog.de\0"
-#define UHASHTOOLS_RC_LEGALCOPYRIGHT_STR   L"\x00a9 2024 Marcel Gosmann\0"
+#define UHASHTOOLS_RC_LEGALCOPYRIGHT_STR   L"\x00a9 2024-2025 Marcel Gosmann\0"
 
 /*
  * Because this file is included by a resource file, this file must
